4a09e756cb5ce45a04095b99dd79ea4b86e42ae1
YahyaBekirCanevi <93769780+YahyaBekirCanevi@users.noreply.github.com>
2025-02-05 01:18:02 +0300

    webform

--- main.go
+++ main.go
@@ -2,25 +2,56 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	//"path/filepath"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"path/filepath"
 )
 
-func main() {
-	fmt.Println("hello world")
-	r := gin.Default()
-	//versioned := r.Group("/v1/")
+type Submission struct {
+	FirstName     string
+	LastName      string
+	PhotoFilename string
+}
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Success")
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
+	r.Static("/uploads", "./uploads")
+	// Route to display the form
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "form.html", nil)
+	})
+
+	// Route to handle form submission
+	r.POST("/submit", func(c *gin.Context) {
+		firstName := c.PostForm("first_name")
+		lastName := c.PostForm("last_name")
+		// Handling file upload
+		file, err := c.FormFile("photo")
+
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+			return
+		}
+
+		// Save the file to the uploads directory
+		filename := filepath.Base(file.Filename)
+
+		if err := c.SaveUploadedFile(file, "uploads/"+filename); err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+			return
+		}
+
+		submission := Submission{
+			FirstName:     firstName,
+			LastName:      lastName,
+			PhotoFilename: filename,
+		}
+
+		c.HTML(http.StatusOK, "result.html", submission)
 	})
 	// Run the server
-	err := r.Run(":8080")
-
-	if err != nil {
-		log.Fatalln("failed to run Gin app")
-	}
+	r.Run(":8080")
 }
